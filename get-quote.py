4d326ae035ba4899ad55eff52eaee122cd29bcd3
Emin Akant <eakant@msn.com>
2021-11-02 22:43:49 +0300

    last Line

--- get-quote.py
+++ get-quote.py
@@ -1,13 +1,13 @@
 def primary():
 
+    print("Keep it logically awesome.")
 
-  print("Keep it logically awesome.")
+    f = open("quotes.txt")
+    quotes = f.readlines()
+    f.close()
 
-#f = open("quotes.txt")
-#quotes = f.readlines()
-# f.close()
+    print(quotes[-1])
 
-# print(quotes)
 
 if __name__ == "__main__":
     primary()
